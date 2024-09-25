4cbabb60f20379676325e0419b9f2ee5d48f9c93
Anthony Byrd <scrill21@gmail.com>
2024-09-24 21:59:56 -0400

    WeSplit project commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by anthony byrd on 11/25/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
