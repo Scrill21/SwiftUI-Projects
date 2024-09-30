57aea360814eb22e9263b0c96673462527881c78
Anthony Byrd <scrill21@gmail.com>
2024-09-29 22:58:03 -0400

    initial commit

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by anthony byrd on 9/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Hello, world!")
+            Text("This is another text view")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
