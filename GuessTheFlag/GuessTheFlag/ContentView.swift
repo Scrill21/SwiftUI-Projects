ef152a3cbfeedc12dd71417ccdcf10df1905f26b
Anthony Byrd <scrill21@gmail.com>
2024-10-22 21:50:00 -0400

    added assets to project

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,10 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("Hello, world!")
-            Text("This is another text view")
+        VStack {
+            Text("Tap the flag of")
+            Text(countries[correctAnswer])
+        }
+        
+        ForEach(0..<3) { number in
+            Button {
+                // flag was tapped
+            } label: {
+                Image(countries[number])
+            }
         }
     }
 }
