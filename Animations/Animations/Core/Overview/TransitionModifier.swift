7cd6169651b714b697918cefed74d81ee0ce2a66
Anthony Byrd <scrill21@gmail.com>
2025-06-24 17:20:40 -0400

    Completed Animations

---
+++ Animations/Animations/Core/Overview/TransitionModifier.swift
@@ -0,0 +1,34 @@
+//
+//  TransitionModifier.swift
+//  Animations
+//
+//  Created by anthony byrd on 6/24/25.
+//
+
+import SwiftUI
+
+struct TransitionModifier: View {
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+//                    .transition(.scale)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }
+    }
+}
+
+#Preview {
+    TransitionModifier()
+}
