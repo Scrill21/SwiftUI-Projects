d88415ee4eeba0de7651c4663aa8c82e00e5c689
Anthony Byrd <scrill21@gmail.com>
2025-06-25 11:51:50 -0400

    Initial project commit
    - Setup project structure

---
+++ iExpense/iExpense/App/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by anthony byrd on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            iExpenseView()
+        }
+    }
+}
---
+++ iExpense/iExpense/Core/iExpenseView.swift
@@ -0,0 +1,24 @@
+//
+//  iExpenseView.swift
+//  iExpense
+//
+//  Created by anthony byrd on 6/24/25.
+//
+
+import SwiftUI
+
+struct iExpenseView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    iExpenseView()
+}
