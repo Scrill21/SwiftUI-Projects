3c6333ef7eebb69a7e89bb9c40cd53e85efdcea0
Anthony Byrd <scrill21@gmail.com>
2025-06-27 13:43:04 -0400

    Initial Project commit
    - Setup file structure
    - Added project image assets

---
+++ Moonshot/Moonshot/Core/Overview/ImageScaling.swift
@@ -0,0 +1,18 @@
+//
+//  ImageContentSize.swift
+//  Moonshot
+//
+//  Created by anthony byrd on 6/27/25.
+//
+
+import SwiftUI
+
+struct ImageScaling: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageScaling()
+}
