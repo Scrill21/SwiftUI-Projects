a7c61721c270cb32846568a9311e6e89bba9359a
Anthony Byrd <scrill21@gmail.com>
2025-06-26 23:16:01 -0400

    Completed iExpense Project

--- iExpense/iExpense/Core/Project/Views/ExpensesView.swift
+++ iExpense/iExpense/Core/Project/Views/ExpensesView.swift
@@ -9,21 +9,35 @@ import SwiftUI
 
 struct ExpensesView: View {
     @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items, id: \.name) { item in
-                    Text(item.name)
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
                 }
                 .onDelete(perform: removeItems)
             }
             .navigationTitle("iExpense")
             .toolbar {
                 Button("Add Expense", systemImage: "plus") {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
         }
     }
