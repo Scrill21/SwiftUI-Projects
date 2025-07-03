89c64b16610fd60954acb7fc9fbfcb0eab4c2e0e
Anthony Byrd <scrill21@gmail.com>
2025-07-03 15:03:53 -0400

    Initial project commit
    - Setup file structure

---
+++ Bookworm/Bookworm/App/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by anthony byrd on 7/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
