072cd1b52e0e92017256cd4fb5eb1df5b64a840a
Anthony Byrd <scrill21@gmail.com>
2025-07-04 00:18:49 -0400

    Working on project overview
    **Covered**:
    - `@Binding` property wrapper
    - `TextEditor` and Dynamic `TextField` views
    - Working on `SwiftData` example

---
+++ Bookworm/Bookworm/Core/Overview/SwiftDataExample.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftDataExample.swift
+//  Bookworm
+//
+//  Created by anthony byrd on 7/4/25.
+//
+
+import SwiftUI
+
+struct SwiftDataExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftDataExample()
+}
