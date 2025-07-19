aee671c53e5431af5ab362b49dc276edbacac9a1
Anthony Byrd <scrill21@gmail.com>
2025-07-19 02:30:42 -0400

    Update BookDetailView.swift
    - Implemented delete book feature

--- Bookworm/Bookworm/Core/Project/Views/BookDetailView.swift
+++ Bookworm/Bookworm/Core/Project/Views/BookDetailView.swift
@@ -9,6 +9,11 @@ import SwiftData
 import SwiftUI
 
 struct BookDetailView: View {
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var showingDeleteAlert = false
+    
     let book: Book
     
     var body: some View {
@@ -40,6 +45,26 @@ struct BookDetailView: View {
         .navigationTitle(book.title)
         .navigationBarTitleDisplayMode(.inline)
         .scrollBounceBehavior(.basedOnSize)
+        .alert("Delete Book", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Delete this book", systemImage: "trash") {
+                    showingDeleteAlert = true
+                }
+            }
+        }
+    }
+}
+
+extension BookDetailView {
+    func deleteBook() {
+        modelContext.delete(book)
+        dismiss()
     }
 }
 
