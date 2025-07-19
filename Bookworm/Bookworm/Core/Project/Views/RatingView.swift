7dcd046b07d8f0b7deab29b2e9166fe4185be2a4
Anthony Byrd <scrill21@gmail.com>
2025-07-19 00:02:49 -0400

    `RatingsView` Implementation
    - Implemented custom `RatingsView` ui component.

---
+++ Bookworm/Bookworm/Core/Project/Views/RatingView.swift
@@ -0,0 +1,55 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by anthony byrd on 7/17/25.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var label = ""
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            
+            Spacer()
+            
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
+                Button {
+                    rating = number
+                } label: {
+                    image(for: number)
+                        .foregroundStyle(number > rating ? offColor : onColor)
+                }
+            }
+        }
+        .buttonStyle(.plain)
+    }
+}
+
+extension RatingView {
+    func image(for number: Int) -> Image {
+        if number > rating {
+            offImage ?? onImage
+        } else {
+            onImage
+        }
+    }
+}
+
+#Preview {
+    RatingView(rating: .constant(4))
+}
