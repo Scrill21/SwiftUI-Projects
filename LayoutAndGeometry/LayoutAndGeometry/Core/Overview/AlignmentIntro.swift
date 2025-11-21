e3291ba5e79295b4e90cab95d56e676367d311b0
Anthony Byrd <scrill21@gmail.com>
2025-11-20 22:21:43 -0500

    Alignments
    - Intro to alignments in SwiftUI

--- LayoutAndGeometry/LayoutAndGeometry/Core/Overview/AlignmentIntro.swift
+++ LayoutAndGeometry/LayoutAndGeometry/Core/Overview/AlignmentIntro.swift
@@ -9,14 +9,16 @@ import SwiftUI
 
 struct AlignmentIntro: View {
     var body: some View {
+        customAlignmentGuide
+    }
+    
+    var basicAlignment: some View {
         Text("Live long and prosper")
             .frame(width: 300, height: 300, alignment: .topLeading)
-        
-        stackAlignment
     }
     
     var stackAlignment: some View {
-        HStack(alignment: .top) {
+        HStack(alignment: .lastTextBaseline) {
             Text("Live")
                 .font(.caption)
             
@@ -28,6 +30,33 @@ struct AlignmentIntro: View {
             Text("prosper")
                 .font(.largeTitle)
         }
+    }
+    
+    var alignmentGuide: some View {
+        VStack(alignment: .leading) {
+            Text("Hello, world!")
+                .alignmentGuide(.leading) { dimension in
+                    dimension[.trailing]
+                }
+            Text("This is a longer line of text")
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
+    }
+    
+    var customAlignmentGuide: some View {
+        VStack(alignment: .leading) {
+            ForEach(0..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading) { _ in
+                        Double(position) * -10
+                    }
+            }
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
     }
 }
 
